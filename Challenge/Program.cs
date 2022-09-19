595db955274d015a98216700366573ae3637cfbf
DenisS <ak5c@mail.ru>
2022-09-19 09:08:07 +0300

    Добавьте файлы проекта.

---
+++ Challenge/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName;
+            FirstName = "Den";
+
+            string LastName;
+            LastName = "Shitov";
+
+            string Country;
+            Country = "Russia";
+
+            string zipCode;
+            zipCode = "344022";
+
+            string City;
+            City = "Rostov on Don";
+
+            string streetAddress;
+            streetAddress = "Juravlev st. 100";
+
+            Console.WriteLine("First Name:  {0}" ,FirstName);
+            Console.WriteLine("Lastname: {0}", LastName); 
+            Console.WriteLine("Country: "+ Country);
+            Console.WriteLine("City: "+ City);
+            Console.WriteLine("Zip Code : "+ zipCode);
+            Console.WriteLine("Street address: "+ streetAddress);
+            Console.ReadLine();
+        }
+    }
+}
